6436ef2ede5731987f150d9c570ed6dcaf999f71
Gabriel S <150805248+shiige05@users.noreply.github.com>
2024-05-22 12:54:34 -0300

    Add files via upload

---
+++ ControladorLutador.java
@@ -0,0 +1,13 @@
+package aula20luta;
+
+/**
+ *
+ * @author gabri
+ */
+public interface ControladorLutador {
+    public abstract void apresentar();
+    public abstract void status();
+    public abstract void ganharLuta();
+    public abstract void perderLuta();
+    public abstract void empatarLuta();
+}
